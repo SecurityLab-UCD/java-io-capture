0e39fc5ecebd4521217fde78c977fbea0309a207
Yifeng He <yfhe@ucdavis.edu>
2023-08-11 15:42:13 +0800

    make example project a sub module

---
+++ example/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world from java-io-capture example project!");
+        int z = add(1, 2);
+        System.out.println(z);
+    }
+}
