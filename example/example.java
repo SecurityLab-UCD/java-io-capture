102e92aa337391a15d77d6b979e3314af9819e45
Yifeng He <yfhe@ucdavis.edu>
2023-08-14 11:37:02 +0800

    add example as a subdir instead as sub-module

--- example/example.java
+++ example/example.java
@@ -1,9 +1,9 @@
-package org.example;
 
-public class Main {
+public class example {
     public static int add(int x, int y) {
         return x + y;
     }
+
     public static void main(String[] args) {
         System.out.println("Hello world from java-io-capture example project!");
         int z = add(1, 2);
