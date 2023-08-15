2f17a47f7809367d9841536aa402df528beab474
Yifeng He <yfhe@ucdavis.edu>
2023-08-15 17:16:54 +0800

    modify interceptor to get method name when exit

--- src/main/java/com/agent/Agent.java
+++ src/main/java/com/agent/Agent.java
@@ -12,10 +12,27 @@ public class Agent {
         System.err.println("Agent premain");
         new AgentBuilder.Default()
                 .type(ElementMatchers.any())
-                .transform((builder, typeDescription, classLoader, javaModule, protectionDomain) -> builder
+                .transform((builder, typeDescription, classLoader, module) -> builder
                         .method(ElementMatchers.any())
                         .intercept(Advice.to(Interceptor.class)))
                 .installOn(inst);
+    }
+
+    public static class Interceptor {
+        @Advice.OnMethodEnter
+        static void onEnter(@Advice.Origin String method, @Advice.AllArguments Object[] args) {
+            System.out.println("Entering method: " + method);
+            System.out.println("Arguments: ");
+            for (Object arg : args) {
+                System.out.println(arg);
+            }
+        }
+
+        @Advice.OnMethodExit
+        static void onExit(@Advice.Origin String method, @Advice.Return(readOnly = false) Object returned) {
+            System.out.println("Exiting method: " + method);
+            System.out.println("Returned value: " + returned);
+        }
     }
 
 }
