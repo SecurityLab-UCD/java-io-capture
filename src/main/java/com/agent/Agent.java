e357ac3f439899943ff190351cf47f5598ff420b
Yifeng He <yfhe@ucdavis.edu>
2023-08-15 21:52:14 +0800

    use MethodDelegation rather than Advice to wrap around function call

--- src/main/java/com/agent/Agent.java
+++ src/main/java/com/agent/Agent.java
@@ -1,26 +1,41 @@
 package com.agent;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
-import net.bytebuddy.utility.JavaModule;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.SuperCall;
+import net.bytebuddy.implementation.bind.annotation.This;
 
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.err.println("Agent premain");
         new AgentBuilder.Default()
                 .type(ElementMatchers.any())
                 .transform((builder, typeDescription, classLoader, module) -> builder
                         .method(ElementMatchers.any())
-                        .intercept(Advice.to(Interceptor.class)))
+                        .intercept(MethodDelegation.to(Interceptor.class)))
                 .installOn(inst);
     }
 
+    // todo
+    // 1. dereference for "pointer" types (like String)
+    // 2. handle exceptions
+    // 3. fix reporting format
     public static class Interceptor {
-        @Advice.OnMethodEnter
-        public static void onEnter(@Advice.Origin String method, @Advice.AllArguments Object[] args) {
+        @RuntimeType
+        public static Object intercept(
+                @This(optional = true) Object origin,
+                @Origin(cache = false) Method method,
+                @SuperCall Callable<?> callable,
+                @AllArguments Object[] args) throws Exception {
+
+            // 1. report input arguments
             System.err.printf("======= Entering method: %s =======>\n", method);
             System.err.println("Arguments: ");
             for (Object arg : args) {
@@ -29,19 +44,22 @@ public class Agent {
                 System.err.println(arg + ": " + arg.getClass());
             }
             System.err.println("<======================================");
-        }
 
-        @Advice.OnMethodExit
-        static void onExit(@Advice.Origin String method) {
-            // !
-            // fix: if this parameter is added, whole instrumentation will not work
-            // public static void onExit(@Advice.Origin String method,
-            // @Advice.Return(readOnly = true) boolean returned) {
+            // 2. call the original method
+            Object rnt = callable.call();
+
+            // 3. report return value
             System.err.printf("------ Exiting method: %s ------->\n", method);
             System.err.println("Returned value: ");
-            // System.err.println(rnt + ": " + rnt.getClass());
+            if (rnt == null) {
+                System.err.println("null");
+            } else {
+                System.err.println(rnt + ": " + rnt.getClass());
+            }
             System.err.println("<--------------------------------------");
-        }
 
+            // 4. return the original return value
+            return rnt;
+        }
     }
 }
